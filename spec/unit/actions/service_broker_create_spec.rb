47da241254ee671df747ca818ca57b1670b79fc5
Oleksii Fedorov <ofedorov@pivotal.io>
2019-09-25 18:05:21 +0200

    Extract Stepper helper for concurrency testing.
    Still TODO:
    - [ ] unit-test the helper code separately
    - [ ] Add `stepper: true` to concurrency tests
    - [ ] Make sure `stepper: true` is ignored during normal unit test run
    - [ ] Add a special rake task to run these tests
    - [ ] Setup CI to run this task nightly
    
    [#168720975]

--- spec/unit/actions/service_broker_create_spec.rb
+++ spec/unit/actions/service_broker_create_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'actions/service_broker_create'
+require 'support/stepper'
 
 module VCAP
   module CloudController
@@ -58,161 +59,45 @@ module VCAP
         expect(broker.service_broker_state.state).to eq(ServiceBrokerStateEnum::SYNCHRONIZING)
       end
 
-      before do
-        ServiceBroker.db.transaction do
-          ServiceBroker.all.each do |broker|
-            puts broker.inspect
-            broker.delete
-          end
-        end
-      end
-
       describe 'concurrent behaviour' do
-        let(:stepper) { Stepper.new }
+        let(:stepper) { Stepper.new(self) }
 
         before do
-          allow(ServiceBroker).to receive(:create).and_wrap_original do |m, params|
-            stepper.step 'start create broker transaction'
-            result = m.call(params)
-            stepper.step 'finish create broker and start create broker state'
-            result
-          end
+          stepper.instrument(
+            ServiceBroker, :create,
+            before: 'start create broker transaction',
+            after: 'finish create broker and start create broker state'
+          )
 
-          allow(ServiceBrokerState).to receive(:create).and_wrap_original do |m, params|
-            result = m.call(params)
-            stepper.step 'finish create broker transaction'
-            result
-          end
-        end
-
-        def interleave_randomly(xs, ys)
-          result = []
-          while !xs.empty? && !ys.empty?
-            choice = [xs, ys].sample
-            result.push(choice.shift)
-          end
-
-          result.push(xs.shift) until xs.empty?
-          result.push(ys.shift) until ys.empty?
-
-          result
-        end
-
-        5.times do |i|
-          it "interleave_randomly works okay #{i}" do
-            result = interleave_randomly([
-              '[1] start create broker transaction',
-              '[1] finish create broker transaction',
-              '[1] start create broker state transaction',
-              '[1] finish create broker state transaction',
-            ], [
-              '[2] start create broker transaction',
-              '[2] finish create broker transaction',
-              '[2] start create broker state transaction',
-              '[2] finish create broker state transaction',
-            ])
-
-            expect(result).to have(8).items
-          end
+          stepper.instrument(
+            ServiceBrokerState, :create,
+            after: 'finish create broker transaction'
+          )
         end
 
         20.times do |i|
           it "works when parallel brokers are created #{i}", isolation: :truncation do
-            errors = []
-            stepper.expected_order = interleave_randomly([
-              '[1] start create broker transaction',
-              '[1] finish create broker and start create broker state',
-              '[1] finish create broker transaction',
-            ], [
-              '[2] start create broker transaction',
-              '[2] finish create broker and start create broker state',
-              '[2] finish create broker transaction',
-            ])
+            stepper.start_thread([
+              'start create broker transaction',
+              'finish create broker and start create broker state',
+              'finish create broker transaction',
+            ]) { subject.create(message) }
 
-            puts
-            puts '===='
-            puts stepper.expected_order
-            puts '===='
-            puts
+            stepper.start_thread([
+              'start create broker transaction',
+              'finish create broker and start create broker state',
+              'finish create broker transaction',
+            ]) { subject.create(message2) }
 
-            t1 = Thread.start do
-              subject.create(message)
-            rescue => e
-              puts e
-              stepper.abort!
-              errors << e
-            end
-            t1.name = '1'
+            stepper.interleave_order
+            stepper.print_order
+            stepper.run
 
-            t2 = Thread.start do
-              subject.create(message2)
-            rescue => e
-              puts e
-              stepper.abort!
-              errors << e
-            end
-            t2.name = '2'
-
-            t2.join
-            t1.join
-
-            expect(errors).to be_empty
+            expect(stepper.errors).to be_empty
+            expect(stepper.steps_left).to be_empty
           end
         end
       end
     end
-  end
-end
-
-class Stepper
-  MAX_RETRIES = 1000
-  attr_accessor :expected_order, :aborted, :mutex
-
-  def initialize
-    @expected_order = []
-    @aborted = false
-    @mutex = Mutex.new
-  end
-
-  def step(message, &block)
-    full_message = "[#{Thread.current.name}] #{message}"
-    puts("expecting #{full_message}")
-
-    retries = 0
-    sleep(0.01) while top_expected_message != full_message && (retries += 1) < MAX_RETRIES && !aborted
-
-    raise "Step #{full_message} has reached max #{retries} retries" if retries >= MAX_RETRIES
-
-    raise 'Aborted' if aborted
-
-    advance_to_next_message
-    block.call if block
-
-    puts("done #{full_message}")
-  end
-
-  def abort!
-    self.aborted = true
-  end
-
-  private
-
-  def top_expected_message
-    mutex.lock
-    result = expected_order.first
-    mutex.unlock
-    result
-  end
-
-  def advance_to_next_message
-    mutex.lock
-    expected_order.shift
-    mutex.unlock
-  end
-end
-
-class StockStepper
-  def step(message, &block)
-    block.call if block
   end
 end
